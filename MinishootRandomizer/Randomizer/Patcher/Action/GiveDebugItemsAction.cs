2d24a3c947969d4a76004668c25f55fc7143453d
TheNooodle <axel.veber+nouillet@gmail.com>
2026-09-06 02:01:46 +0200

    Fix Sunken Temple entrance

--- MinishootRandomizer/Randomizer/Patcher/Action/GiveDebugItemsAction.cs
+++ MinishootRandomizer/Randomizer/Patcher/Action/GiveDebugItemsAction.cs
@@ -28,7 +28,9 @@ public class GiveDebugItemsAction : IPatchAction
 
         PlayerState.SetSkill(Skill.Boost, true);
         PlayerState.SetSkill(Skill.Dash, true);
-        // PlayerState.SetSkill(Skill.Supershot, true);
+        PlayerState.SetSkill(Skill.Supershot, true);
+        WorldState.Set("BlastshotUnlocked", true);
+        // WorldState.Set("FlameshotUnlocked", true);
         PlayerState.SetSkill(Skill.Hover, true);
         WorldState.Set($"{WaterType.Normal}HoverUnlocked", true);
         WorldState.Set($"{WaterType.Blue}HoverUnlocked", true);
